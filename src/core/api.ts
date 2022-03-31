cbb8146fd4f3c0f711137a7dcbd7c91dfa7c1290
Alex Bennett <alex.bennett@hideandseek.digital>
2022-03-31 17:34:33 +1100

    Updated SDK to match docs and finished base implementation

--- src/core/api.ts
+++ src/core/api.ts
@@ -1,56 +1,23 @@
 import { AxiosInstance, AxiosRequestConfig } from "axios";
 
-import { Session, SessionConfig, Data, Navigate, StepId } from "./types";
-import { buildUrl } from "./util";
+import { Session, SessionConfig, Data, Navigate, StepId, SessionId, ProjectId } from "./types";
 
-const checkStatus = (res: any) => {
-  console.log('checking res status', res);
-  return JSON.parse(res);
-  // if (res.ok) {
-  //   return res;
-  // }
-  // throw (res);
-};
-
-export const create = async (api: AxiosInstance, project: string, options: SessionConfig = {}) => {
+export const create = async (api: AxiosInstance, project: ProjectId, options: SessionConfig = {}) => {
   const { initialData, autogen, interview, release } = options;
-  const config: AxiosRequestConfig = {};
+  const config: AxiosRequestConfig = {
+    params: {
+      release,
+      interview
+    }
+  };
 
-  if(release) {
-    config.params = { release };
-  }
-
-  let url = project;
-  if(interview) {
-    url = buildUrl(url, interview);
-  }
-  
-  // returns session id and screen info
-  const res = await api.post<Session>(url, { data: initialData, autogen }, config);
+  const res = await api.post<Session>(project, { data: initialData, autogen }, config);
   return res.data;
-
-  // const config: AxiosRequestConfig = {
-  //   params: {
-  //     interview,
-  //     release
-  //   }
-  // };
-
-  // // returns session id and screen info
-  // const res = await api.post<Session>(project, { data: initialData, autogen }, config);
-  // return res.data;
 };
 
-export const load = async (api: AxiosInstance, project: string, session: string) => {
-  try {
-    const url = buildUrl('progress', project);
-    const res = await api.post<Session>(url, { mode: 'interview2.0' }, { params: { interview: session } });
-    console.log('loaded session', res);
-    // return new Session(api, project, res.data);
+export const load = async (api: AxiosInstance, project: ProjectId, session: SessionId) => {
+    const res = await api.get<Session>(project, { params: { session } });
     return res.data;
-  } catch (err) {
-    console.log('api failed, load', err);
-  }
 };
 
 /**
@@ -59,11 +26,8 @@ export const load = async (api: AxiosInstance, project: string, session: string)
  * @param data The data for the current step to submit
  * @param navigate The desired navigation after update, defaults to next
  */
-export const submit = async (api: AxiosInstance, project: string, session: string, data: Data, navigate: Navigate = true) => {
-  const url = buildUrl('progress', project);
-
-  // TODO should be patch??
-  const res = await api.post<Session>(url, { mode: 'interview2.0', data, navigate }, { params: { interview: session } });
+export const submit = async (api: AxiosInstance, project: ProjectId, session: SessionId, data: Data, navigate: Navigate = true) => {
+  const res = await api.patch<Session>(project, { data, navigate }, { params: { session: session } });
   return res.data;
 };
 
@@ -72,105 +36,7 @@ export const submit = async (api: AxiosInstance, project: string, session: strin
  * 
  * @param step The desired step ID
  */
-export const navigate = async (api: AxiosInstance, project: string, session: string, step: StepId) => {
-  const url = buildUrl('progress', project);
-  const res = await api.patch<Session>(url, { mode: 'interview2.0', navigate: step }, { params: { interview: session } });
+export const navigate = async (api: AxiosInstance, project: ProjectId, session: SessionId, step: StepId) => {
+  const res = await api.patch<Session>(project, { navigate: step }, { params: { session } });
   return res.data;
 };
-
-// export class Session {
-//   private api: AxiosInstance;
-//   private project: string;
-//   private _state: SessionState;
-  
-//   get id() {
-//     return this._state.session;
-//   };
-
-//   get state() {
-//     return this._state;
-//   }
-
-//   /**
-//    * Create a new session instance
-//    * 
-//    * @param project The project ID
-//    */
-//   constructor(api: AxiosInstance, project: string, state: SessionState) {
-//     this.api = api;
-//     this.project = project;
-//     this._state = state;
-//   }
-
-//   /**
-//    * Create a new session for an interview.
-//    * 
-//    * @param interview The desired interview ID. Null to use default interview.
-//    * @param options 
-//    * @param release Target release to test against
-//    * @returns 
-//    */
-//   // create = async (interview: string | null, options?: CreateSessionOptions, release?: string) => {
-//   //   const data = options ?? {};
-//   //   const config: AxiosRequestConfig = {};
-
-//   //   if(release) {
-//   //     config.params = { release };
-//   //   }
-
-//   //   let url = this.model;
-//   //   if(interview) {
-//   //     url = [url, interview].join('/');
-//   //   }
-
-//   //   const res = await this.api.post<SessionState>(url, data, config);
-//   //   this.session = res.data.session;
-//   //   return res.data;
-//   // }
-
-//   /**
-//    * Gets the sessions current state.
-//    *
-//    * @param session An existing session ID
-//    */
-//   // load = async (session: string) => {
-//   //   // return this.api.get(this.model, { params: { id: this.interview, release: this.release } });
-//   //   const url = this.buildUrl(this.model, session);
-//   //   const res = await this.api.get<SessionState>(url);
-//   //   this.session = session;
-//   //   return res.data;
-//   // };
-
-//   /**
-//    * Save the data but do not progress to the next step.
-//    */
-//   save = (data: Data) => {
-//     return this.submit(data, false);
-//   };
-  
-//   /**
-//    * Submit response for current step.
-//    * 
-//    * @param data The data for the current step to submit
-//    * @param navigate The desired navigation after update, defaults to next
-//    */
-//   submit = async (data: Data, navigate: Navigate = true) => {
-//     const url = buildUrl(this.project, this.id);
-//     const res = await this.api.patch<SessionState>(url, { ...data, navigate });
-//     return res.data;
-//   };
-
-//   /**
-//    * Navigate to a specific step.
-//    * 
-//    * @param step The desired step ID
-//    */
-//   navigate = async (step: StepId) => {
-//     const url = buildUrl(this.project, this.id);
-//     const res = await this.api.patch<SessionState>(url, { navigate: step });
-//     return res.data;
-//   };
-
-// };
-
-// export type SessionFactory = (model: string) => Session;
--- src/core/init.ts
+++ src/core/init.ts
@@ -1,28 +1,56 @@
-import axios from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosRequestTransformer, AxiosResponseTransformer } from "axios";
 import { buildUrl } from "./util";
 import { create, load, submit, navigate } from './api';
-import { SessionConfig, Data, Navigate, StepId } from './types';
+import { SessionConfig, Data, StepId, SessionInstance } from './types';
+
+const MODE = 'interview2.0';
+
+export const createApiInstance = (baseURL: string, overrides: AxiosRequestConfig = {}) => {
+  return axios.create({
+    baseURL,
+    timeout: 30000,
+    headers: { 'Content-Type': 'application/json' },
+    transformRequest: [
+      (req) => {
+        // inject mode onto request
+        req.mode = MODE;
+        return req;
+      },
+      ...axios.defaults.transformRequest as AxiosRequestTransformer[]
+    ],
+    ...overrides
+  });
+};
+
+// should submit apply any context? depends on collection approach??
+
+const createSessionTransform = (api: AxiosInstance, project: string): AxiosResponseTransformer => (res) => {
+  res._api = api;
+  res.submit = (data: Data, navigate = true) => submit(api, project, res.sessionId, data, navigate);
+  res.save = (data: Data) => submit(api, project, res.sessionId, data, false);
+  res.navigate = (step: StepId) => navigate(api, project, res.sessionId, step);
+  return res;
+};
 
 export const init = (host: string, env: string = 'test') => {
-    const api = axios.create({
-      baseURL: buildUrl(host, 'example', 'decisionapi', env),
-      timeout: 30000,
-      headers: { 'Content-Type': 'application/json' },
-      // already passed 200 check
-      transformResponse: [(res) => {
-        res._api = api;
-        res.submit = (data: Data, navigate = true) => submit(api, '', res.sessionId, data, navigate);
-        res.save = (data: Data) => submit(api, '', res.sessionId, data, false);
-        res.navigate = (step: StepId) => navigate(api, '', res.sessionId, step);
-        return res;
-      }]
-    });
-  
-    // return session factory
-    // const factory: SessionFactory = (model) => new Session(api, model);
-    // return factory;
-    return {
-        create: (project: string, config: SessionConfig) => create(api, project, config),
-        load: (project: string, session: string) => load(api, project, session)
-    };
+  const baseUrl = buildUrl(host, 'example', 'decisionapi', env, 'progress');
+  const api = createApiInstance(baseUrl);
+
+  const transformApi = (project: string) => {
+    api.defaults.transformResponse = [
+        ...axios.defaults.transformResponse as AxiosResponseTransformer[],
+        createSessionTransform(api, project)
+    ];
   };
+
+  return {
+    create: (project: string, config: SessionConfig) => {
+      transformApi(project);
+      return create(api, project, config) as Promise<SessionInstance>;
+    },
+    load: (project: string, sessionId: string) => {
+      transformApi(project);
+      return load(api, project, sessionId) as Promise<SessionInstance>;
+    }
+  }
+};
